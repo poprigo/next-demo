557c97f351cb4b1fdd5ac0ae6ffbaefa806d8573
Ankit Detroja <47050795+AKKIDETROJA@users.noreply.github.com>
2024-04-29 13:30:18 +0530

    modify next config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    
+    basePath: '/github-pages',
+};
 
 export default nextConfig;
+
