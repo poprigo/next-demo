07ee1360963793dfbdc803c76072d02f130ea626
Ankit Detroja <47050795+AKKIDETROJA@users.noreply.github.com>
2024-04-29 13:37:17 +0530

    modify next config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    
-    basePath: '/github-pages',
+
+    basePath: '/poprigo.github.io',
+    // images: {
+    //     domains: ['poprigo.github.io'],
+    // },
 };
 
 export default nextConfig;
